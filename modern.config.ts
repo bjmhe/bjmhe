911ecf046ddc037ccff859a193ce5db6cda76768
hyq <betterhyq@qq.com>
2026-02-01 16:20:59 +0800

    feat: set the project

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,8 @@
 import { SemiRspackPlugin } from '@douyinfe/semi-rspack-plugin';
 import { appTools, defineConfig } from '@modern-js/app-tools';
+import { polyfillPlugin } from '@modern-js/plugin-polyfill';
 import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
+import { pluginImageCompress } from '@rsbuild/plugin-image-compress';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
@@ -12,7 +14,9 @@ export default defineConfig({
       bundler: 'rspack',
     }),
     tailwindcssPlugin(),
+    polyfillPlugin(),
   ],
+  builderPlugins: [pluginImageCompress()],
   tools: {
     rspack: {
       plugins: [new SemiRspackPlugin({})],
